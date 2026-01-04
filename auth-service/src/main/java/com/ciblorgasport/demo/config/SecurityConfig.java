961b083c799abf10ed10f0ca1a7fe211f729209e
adama <adama.coundoul.etu@univ-lille.com>
2026-01-04 20:46:44 +0100

    fix CORS security problems for render

--- auth-service/src/main/java/com/ciblorgasport/demo/config/SecurityConfig.java
+++ auth-service/src/main/java/com/ciblorgasport/demo/config/SecurityConfig.java
@@ -11,10 +11,16 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity // pour @PreAuthorize
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
@@ -27,18 +33,42 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
-            .cors(cors -> {})
+            
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            
             .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/**").permitAll()
+               
                 .requestMatchers("/api/admin/**").hasRole("RESPONSABLE")
                 .requestMatchers("/api/competitions/**").hasAnyRole("RESPONSABLE","COMMISSAIRE")
                 .requestMatchers("/api/spectateur/**").hasRole("SPECTATEUR")
+              
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+   
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        
+       
+        configuration.setAllowedOrigins(List.of(
+            "http://localhost:1002",  
+            "https://bcfs-group-ciblorgasport-front.onrender.com" 
+        ));
+        
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
+        configuration.setAllowCredentials(true);
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean

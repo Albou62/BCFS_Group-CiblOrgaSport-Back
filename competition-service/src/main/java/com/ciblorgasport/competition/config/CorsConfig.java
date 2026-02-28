49fc08819fd78b2283eba15763d8dfdb1d274766
mrdedede <andore.filho2000@gmail.com>
2026-02-28 10:06:55 +0100

    feat: ajouter localhost en dev sur CORS

--- competition-service/src/main/java/com/ciblorgasport/competition/config/CorsConfig.java
+++ competition-service/src/main/java/com/ciblorgasport/competition/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:1002")); // front
+        config.setAllowedOrigins(List.of("http://localhost:1002", "http://localhost:5173")); // front
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
